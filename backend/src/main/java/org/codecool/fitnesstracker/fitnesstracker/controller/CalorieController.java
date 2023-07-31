3b6eb02a6cd8307d70cf7c1ce77d0f7ef7bc697c
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-07-31 21:42:25 +0200

    create one get and one post endpoint for  meal and the necessary DTO record and service accordingly

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/CalorieController.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/CalorieController.java
@@ -1,17 +1,30 @@
 package org.codecool.fitnesstracker.fitnesstracker.controller;
 
-import org.codecool.fitnesstracker.fitnesstracker.service.CaloryService;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.CalorieDTO;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewCalorieDTO;
+import org.codecool.fitnesstracker.fitnesstracker.service.CalorieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("calories")
 public class CalorieController {
-    public final CaloryService caloryService;
+    public final CalorieService calorieService;
 
     @Autowired
-    public CalorieController(CaloryService caloryService) {
-        this.caloryService = caloryService;
+    public CalorieController(CalorieService calorieService) {
+        this.calorieService = calorieService;
+    }
+
+    @GetMapping("/all")
+    public List<CalorieDTO> getAllCalories() {
+        return calorieService.getAllCalories();
+    }
+
+    @PostMapping("/")
+    public void addNewMeal(@RequestBody NewCalorieDTO meal) {
+        calorieService.addNewMeal(meal);
     }
 }
--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/CaloryService.java
+++
@@ -1,7 +0,0 @@
-package org.codecool.fitnesstracker.fitnesstracker.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CaloryService {
-}
