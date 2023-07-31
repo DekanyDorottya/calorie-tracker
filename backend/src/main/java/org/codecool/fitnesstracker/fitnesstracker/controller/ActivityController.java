300da54b835b8e825c39bbb67345c3f2fa0027f8
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-07-31 20:08:31 +0200

    create get endpoint for all activity and the necessary DTO record and service accordingly

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/ActivityController.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/ActivityController.java
@@ -1,9 +1,13 @@
 package org.codecool.fitnesstracker.fitnesstracker.controller;
 
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
 import org.codecool.fitnesstracker.fitnesstracker.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("activities")
@@ -15,4 +19,10 @@ public class ActivityController {
     public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
     }
+
+    @GetMapping("/all")
+    public List<ActivityDTO> getAllActivities() {
+        return activityService.getAllActivities();
+    }
+
 }
--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityService.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityService.java
@@ -1,7 +1,15 @@
 package org.codecool.fitnesstracker.fitnesstracker.service;
 
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ActivityService {
+    private List<ActivityDTO> userActivity = new ArrayList<>();
+    public List<ActivityDTO> getAllActivities() {
+        return userActivity;
+    }
 }
