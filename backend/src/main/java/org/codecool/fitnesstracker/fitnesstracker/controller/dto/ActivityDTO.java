0da72da76a6c3ffd0a48549fc01ec10cf6a6e71b
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-08-03 11:18:21 +0200

    test activity

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/ActivityDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/ActivityDTO.java
@@ -1,6 +1,20 @@
 package org.codecool.fitnesstracker.fitnesstracker.controller.dto;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public record ActivityDTO(String activity, int calories, LocalDateTime activityDateTime) {}
+public record ActivityDTO(String activity, int calories, LocalDateTime activityDateTime) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActivityDTO that = (ActivityDTO) o;
+        return calories == that.calories && Objects.equals(activity, that.activity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(activity, calories);
+    }
+}
 
---
+++ backend/src/test/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityServiceTest.java
@@ -0,0 +1,27 @@
+package org.codecool.fitnesstracker.fitnesstracker.service;
+
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewActivityDTO;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityServiceTest {
+
+    @Test
+    void test_getAllActivities() {
+        ActivityService activityService = new ActivityService();
+        activityService.addNewActivity(new NewActivityDTO("Running", 2));
+        List<ActivityDTO> userActivity = new ArrayList<>();
+        userActivity.add(new ActivityDTO("Running", 2, LocalDateTime.now()));
+
+        List<ActivityDTO> actual = activityService.getAllActivities();
+        List<ActivityDTO> expected = userActivity;
+        assertIterableEquals(expected, actual);
+    }
+
+}
