9390108893e4e8d49d618cd7559960756c9ef405
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-06 16:39:58 +0200

    update roles and authorities

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/user/Role.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/user/Role.java
@@ -1,24 +1,39 @@
 package org.codecool.fitnesstracker.fitnesstracker.user;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import static org.codecool.fitnesstracker.fitnesstracker.user.Authorities.*;
+
 public enum Role {
-    USER,
-    ADMIN;
+    USER(new HashSet<>(Set.of(SET_CALORIES,
+            GET_CALORIES,
+            SET_ACTIVITIES,
+            GET_ACTIVITIES,
+            GET_ANALYZE,
+            CHANGE_USER))),
+    ADMIN(new HashSet<>(Set.of(BAN_USER)));
+
+    private final Set<Authorities> authorities;
+
+    Role(Set<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+
+
 
     // example for mapping authorities to roles.
     // Exercise: Make it adhere to Open/Close principle
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        var authorityNames = switch (this) {
-            // FIXME: Don't use had-written string literals
-            case USER -> Set.of("SET_CALORIES", "REMOVE_ACTIVITY");
-            case ADMIN -> Set.of("BAN_USER");
-        };
-        return authorityNames.stream().map(SimpleGrantedAuthority::new).toList();
-    }
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        var authorityNames = switch (this) {
+//            // FIXME: Don't use had-written string literals
+//            case USER -> Set.of("SET_CALORIES", "REMOVE_ACTIVITY");
+//            case ADMIN -> Set.of("BAN_USER");
+//        };
+//        return authorityNames.stream().map(SimpleGrantedAuthority::new).toList();
+//    }
 }
