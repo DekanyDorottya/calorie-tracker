b3941920db1dff3f8a727c7a957b2e3f266103b5
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-10 14:55:46 +0200

    add activityType and update the related fields

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/dao/model/ActivityType.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/dao/model/ActivityType.java
@@ -1,4 +1,25 @@
 package org.codecool.fitnesstracker.fitnesstracker.dao.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String activityType;
+    private int calories;
+
+    @OneToMany(mappedBy = "activityType", cascade = CascadeType.MERGE)
+    private Set<Activity> activities = new HashSet<>();
 }
