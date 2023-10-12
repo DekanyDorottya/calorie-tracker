3f98418d33edd7528d29d0fb1ebb14cf7fbf8e34
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-12 23:34:59 +0200

    create FoodTypeController

---
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/FoodTypeController.java
@@ -0,0 +1,22 @@
+package org.codecool.fitnesstracker.fitnesstracker.controller;
+
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.FoodTypeDTO;
+import org.codecool.fitnesstracker.fitnesstracker.service.FoodTypeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("foodtype")
+public class FoodTypeController {
+    public final FoodTypeService foodTypeService;
+
+    @GetMapping("/search")
+    public List<FoodTypeDTO> getAllFoodType (@RequestParam String foodType) {
+        return foodTypeService.getSearchedFoodType(foodType);
+    }
+
+}
