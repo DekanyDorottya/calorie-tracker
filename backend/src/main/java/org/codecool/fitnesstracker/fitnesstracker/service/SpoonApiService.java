8e3f4257416ea706d2dd599a37d71de6aa5b2b6d
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-16 16:31:45 +0200

    create SpoonApiService

---
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/SpoonApiService.java
@@ -0,0 +1,15 @@
+package org.codecool.fitnesstracker.fitnesstracker.service;
+
+import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpoonApiService {
+
+
+    public List<FoodType> getSearchedFoodTypeFromApi(String foodType) {
+
+    }
+}
