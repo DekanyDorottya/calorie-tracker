156da6db0023156d7948ca596aba4a7d740abb5a
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-16 15:34:29 +0200

    add autowired and static value

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/FoodTypeService.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/FoodTypeService.java
@@ -3,6 +3,7 @@ package org.codecool.fitnesstracker.fitnesstracker.service;
 import org.codecool.fitnesstracker.fitnesstracker.controller.dto.FoodTypeDTO;
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
 import org.codecool.fitnesstracker.fitnesstracker.repositories.FoodTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,10 +12,16 @@ import java.util.List;
 @Service
 public class FoodTypeService {
 
+    public static final char PERCENTAGE_SYMBOL = '%';
     FoodTypeRepository foodTypeRepository;
 
+    @Autowired
+    public FoodTypeService(FoodTypeRepository foodTypeRepository) {
+        this.foodTypeRepository = foodTypeRepository;
+    }
+
     public List<FoodTypeDTO> getSearchedFoodType(String foodType) {
-        List<FoodType> foodTypeList = foodTypeRepository.findCalorieTypeByFoodTypeIsLikeIgnoreCase(foodType);
+        List<FoodType> foodTypeList = foodTypeRepository.findCalorieTypeByFoodTypeIsLikeIgnoreCase(foodType + PERCENTAGE_SYMBOL);
 
         List<FoodTypeDTO> foodTypeDTOS = new ArrayList<>();
         for(FoodType calorieType : foodTypeList) {
