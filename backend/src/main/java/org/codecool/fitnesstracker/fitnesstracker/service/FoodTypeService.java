96eb51e906a3a57089a3f468b4d72df822e64efc
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-16 14:34:46 +0200

    implement search function in the FoodTypeService

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/FoodTypeService.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/FoodTypeService.java
@@ -1,5 +1,26 @@
 package org.codecool.fitnesstracker.fitnesstracker.service;
 
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.CalorieDTO;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.FoodTypeDTO;
+import org.codecool.fitnesstracker.fitnesstracker.dao.model.CalorieType;
+import org.codecool.fitnesstracker.fitnesstracker.repositories.FoodTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class FoodTypeService {
 
+    FoodTypeRepository foodTypeRepository;
+
+    public List<FoodTypeDTO> getSearchedFoodType(String foodType) {
+        List<CalorieType> foodTypeList = foodTypeRepository.findCalorieTypeByFoodTypeIsLikeIgnoreCase(foodType);
+
+        List<FoodTypeDTO> foodTypeDTOS = new ArrayList<>();
+        for(CalorieType calorieType : foodTypeList) {
+            foodTypeDTOS.add(new FoodTypeDTO(calorieType.getCalories(), calorieType.getProtein(), calorieType.getCarbohydrate(), calorieType.getFat()));
+        }
+        return foodTypeDTOS;
+    }
 }
