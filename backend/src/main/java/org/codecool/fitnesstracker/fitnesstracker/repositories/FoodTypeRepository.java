187aa27340b5b07b6120360adfb823ce6c041c1e
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-16 15:34:04 +0200

    add autowired

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/FoodTypeRepository.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/FoodTypeRepository.java
@@ -2,9 +2,10 @@ package org.codecool.fitnesstracker.fitnesstracker.repositories;
 
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface FoodTypeRepository extends JpaRepository<FoodType, Long> {
     List<FoodType> findCalorieTypeByFoodTypeIsLikeIgnoreCase(String foodType);
 }
