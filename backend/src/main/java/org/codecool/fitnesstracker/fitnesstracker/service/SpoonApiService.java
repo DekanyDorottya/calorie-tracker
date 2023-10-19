266871f237faa85adb76fe0824e89f91c3f49c75
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-19 14:52:21 +0200

    update SpoonApiService to get the food detail

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/SpoonApiService.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/SpoonApiService.java
@@ -1,6 +1,7 @@
 package org.codecool.fitnesstracker.fitnesstracker.service;
 
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
+import org.codecool.fitnesstracker.fitnesstracker.data.FoodTypeInfo;
 import org.codecool.fitnesstracker.fitnesstracker.data.FoodTypeResult;
 import org.codecool.fitnesstracker.fitnesstracker.data.FoodTypeTotal;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +11,18 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class SpoonApiService {
+
+    private static final String URL_INGREDIENT_SEARCH = "search?query=";
+    private static final String URL_INGREDIENT_INFORMATION = "/information?amount=100&unit=grams";
 
     @Value("${spoonapi.url}")
     String spoonUrl;
@@ -34,46 +40,28 @@ public class SpoonApiService {
 
     public void getSearchedFoodTypeFromApi(String foodType) {
         System.out.println("belement");
+        Optional <List<FoodTypeResult>> searchedIngredientsById = getListOfFoodTypeIds(foodType);
+
 //        String apiUrl = "https://api.spoonacular.com/food/ingredients/search?query="+foodType;
 //
 //        String response = restTemplate.getForObject(apiUrl, String.class, apiKey);
-        try {
-            URI uri;
-            uri = new URI(spoonUrl+foodType);
-            HttpHeaders headers = new HttpHeaders();
-            headers.set(apiKeyName, apiKeyValue);
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            HttpEntity<String> request = new HttpEntity<String>(headers);
-            ResponseEntity<FoodTypeTotal> responseEntity = restTemplate.exchange(
-                    uri, HttpMethod.GET, request, FoodTypeTotal.class);
-
-            //System.out.println(responseEntity);
-
-            if(responseEntity.getStatusCode() == HttpStatus.OK && responseEntity.getBody() != null) {
-                FoodTypeTotal foodTypeTotal = responseEntity.getBody();
-                System.out.println(foodTypeTotal.results());
+        Optional <List<FoodTypeInfo>> foodTypeInfos;
+        if (searchedIngredientsById.isPresent()) {
+            for (FoodTypeResult foodTypeResult : searchedIngredientsById.get()) {
+                System.out.println(getListOfIngredientInfo(foodTypeResult.id()));
+                //getListOfIngredientInfo(foodTypeResult.id())
             }
 
-//            if (responseEntity.getStatusCode() == HttpStatus.OK && responseEntity.getBody() != null) {
-//                FoodTypeTotal foodTypeTotal = responseEntity.getBody();
-//                List<FoodType> foodTypes = foodTypeTotal.results().stream()
-//                        .map(result -> new FoodType(result.id(), result.name(), result.image()))
-//                        .collect(Collectors.toList());
-//
-//
-//            }
 
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
     }
 
     private Optional<List<FoodTypeResult>> getListOfFoodTypeIds(String foodType) {
 
         try {
+            String encodedQueryParamValue = URLEncoder.encode(foodType, "UTF-8");
             URI uri;
-            uri = new URI(spoonUrl+foodType);
+            uri = new URI(spoonUrl+URL_INGREDIENT_SEARCH+encodedQueryParamValue);
             HttpHeaders headers = new HttpHeaders();
             headers.set(apiKeyName, apiKeyValue);
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -89,10 +77,30 @@ public class SpoonApiService {
             } else {
                 return Optional.empty();
             }
-            } catch (URISyntaxException e) {
+            } catch (URISyntaxException | UnsupportedEncodingException e) {
             e.printStackTrace();
             return Optional.empty();
             }
 
+    }
+
+    private FoodTypeInfo getListOfIngredientInfo(int id) {
+        FoodTypeInfo foodTypeInfo = null;
+        try {
+            URI uri;
+            uri = new URI (spoonUrl+id+URL_INGREDIENT_INFORMATION);
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(apiKeyName, apiKeyValue);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            HttpEntity<String> request = new HttpEntity<>(headers);
+            ResponseEntity<FoodTypeInfo> responseEntity = restTemplate.exchange(
+                    uri, HttpMethod.GET, request, FoodTypeInfo.class
+            );
+            foodTypeInfo = responseEntity.getBody();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return foodTypeInfo;
     }
 }
