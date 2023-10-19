8f6c3c5ae416c17f6ab07c4580882794a4f31109
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-19 22:24:11 +0200

    be renamed

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/data/FoodTypeTotal.java
+++
@@ -1,6 +0,0 @@
-package org.codecool.fitnesstracker.fitnesstracker.data;
-
-import java.util.List;
-
-public record FoodTypeTotal(List<FoodTypeResult> results, int offset, int number, int totalResults) {
-}
