09595d9d47d500292f1fe972ab55b79c7c08fcee
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-19 16:18:19 +0200

    add new exception when there is no search result

---
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/exceptions/NoSuchFoodTypeException.java
@@ -0,0 +1,7 @@
+package org.codecool.fitnesstracker.fitnesstracker.exceptions;
+
+public class NoSuchFoodTypeException extends RuntimeException {
+    public NoSuchFoodTypeException(String message) {
+        super(message);
+    }
+}
