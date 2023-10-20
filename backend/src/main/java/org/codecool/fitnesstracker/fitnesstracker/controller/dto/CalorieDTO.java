82c7f6ecbb466b6412661347917d4ddf3a8412f9
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-20 12:08:58 +0200

    update record fields

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/CalorieDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/CalorieDTO.java
@@ -4,5 +4,5 @@ import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
 
 import java.time.LocalDateTime;
 
-public record CalorieDTO(FoodType foodType, int consumption, LocalDateTime mealDateTime) {
+public record CalorieDTO(String FoodType, double calorie, double carbohydrate, double protein, double fat, int consumption, LocalDateTime mealDateTime) {
 }
