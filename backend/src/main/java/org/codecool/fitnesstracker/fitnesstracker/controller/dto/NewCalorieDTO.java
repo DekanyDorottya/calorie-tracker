c3805b4345c0dcf33036a459e5a773436198d92b
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-20 12:10:46 +0200

    update NewCalorieDTO with api id

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewCalorieDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewCalorieDTO.java
@@ -2,5 +2,7 @@ package org.codecool.fitnesstracker.fitnesstracker.controller.dto;
 
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.FoodType;
 
-public record NewCalorieDTO(FoodType foodType, int consumption) {
+import java.time.LocalDateTime;
+
+public record NewCalorieDTO(long apiId, int consumption, LocalDateTime mealDateTime) {
 }
