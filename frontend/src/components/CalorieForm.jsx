90570a1aeb3e38b8acef1375cbe591cb159569e5
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-22 17:36:41 +0200

    add pointer to list and align button

--- frontend/src/components/CalorieForm.jsx
+++ frontend/src/components/CalorieForm.jsx
@@ -7,15 +7,15 @@ import { Box } from "@mui/material";
 import Cookies from "js-cookie";
 import DailyBarchart from "./DailyBarchart";
 import { Add } from "@mui/icons-material";
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 
 const CalorieForm = () => {
   const [grams, setGrams] = useState(0);
@@ -75,26 +75,25 @@ const CalorieForm = () => {
 
   const fetchFoodType = () => {
     return fetch(`/foodtype/search?&foodType=${searchedFood}`, {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${jwtToken}`
-        },
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
     }).then((res) => res.json());
-  }
+  };
 
   const handleSearchClick = () => {
     fetchFoodType().then((listedFoodTypes) => {
-        setIsSearched(true)
-        setFoundFoodTypes(listedFoodTypes);
-        console.log(listedFoodTypes);
-    })
-  }
+      setIsSearched(true);
+      setFoundFoodTypes(listedFoodTypes);
+      console.log(listedFoodTypes);
+    });
+  };
 
   const handleSelectFoodType = (selectedFoodType) => {
     setFoodType(selectedFoodType);
     setIsSearched(false);
-  }
-
+  };
 
   window.addEventListener("resize", showDailyBarChart);
 
@@ -131,48 +130,51 @@ const CalorieForm = () => {
       <Box flex={5} p={{ xs: 0, md: 2, alignItems: "center" }}>
         {isSearched ? (
           <>
-            <TextField 
-                id="outlined-search" 
-                label="Search food" 
-                type="search" 
-                fullWidth 
-                value={searchedFood} 
-                onChange={(event) => setSearchedFood(event.target.value)}
-                InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={handleSearchClick}>
-                          <SearchIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                
-                />
-                <Table>
-{foundFoodTypes.length !== 0 ? (
-  <TableHead>
-    <TableRow>
-      <TableCell>Food Type (100g serving)</TableCell>
-      <TableCell>Calorie</TableCell>
-      <TableCell>Carbohydrate</TableCell>
-      <TableCell>Fat</TableCell>
-      <TableCell>Protein</TableCell>
-    </TableRow>
-  </TableHead>
-) : null}
-  <TableBody>
-    {foundFoodTypes.map((foodtype) => (
-      <TableRow sx={{  cursor: 'pointer' }} key={foodtype.apiId} onClick={() => handleSelectFoodType(foodtype)}>
-        <TableCell>{foodtype.name}</TableCell>
-        <TableCell>{foodtype.calorie}</TableCell>
-        <TableCell>{foodtype.carbohydrate}</TableCell>
-        <TableCell>{foodtype.fat}</TableCell>
-        <TableCell>{foodtype.protein}</TableCell>
-      </TableRow>
-    ))}
-  </TableBody>
-</Table>
+            <TextField
+              id="outlined-search"
+              label="Search food"
+              type="search"
+              fullWidth
+              value={searchedFood}
+              onChange={(event) => setSearchedFood(event.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleSearchClick}>
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Table>
+              {foundFoodTypes.length !== 0 ? (
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Food Type (100g serving)</TableCell>
+                    <TableCell>Calorie</TableCell>
+                    <TableCell>Carbohydrate</TableCell>
+                    <TableCell>Fat</TableCell>
+                    <TableCell>Protein</TableCell>
+                  </TableRow>
+                </TableHead>
+              ) : null}
+              <TableBody>
+                {foundFoodTypes.map((foodtype) => (
+                  <TableRow
+                    sx={{ cursor: "pointer" }}
+                    key={foodtype.apiId}
+                    onClick={() => handleSelectFoodType(foodtype)}
+                  >
+                    <TableCell>{foodtype.name}</TableCell>
+                    <TableCell>{foodtype.calorie}</TableCell>
+                    <TableCell>{foodtype.carbohydrate}</TableCell>
+                    <TableCell>{foodtype.fat}</TableCell>
+                    <TableCell>{foodtype.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </>
         ) : (
           <>
@@ -195,18 +197,23 @@ const CalorieForm = () => {
             />
             <form className="calorie-form" onSubmit={handleSubmit}>
               <label htmlFor="grams">Amount:</label>
-              <input
+              <TextField
                 type="number"
-                id="grams"
-                value={grams}
+                style={{width: '250px'}}
+                id="outlined-basic"
+                label="Amount"
+                variant="outlined"
                 onChange={handleCaloriesChange}
-                className="gram-input"
-                fullWidth
+                value={grams}
+                sx={{ mb: 2 }}
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">g</InputAdornment>,
+                }}
               />
               <Button
                 variant="contained"
                 type="submit"
-                className="submit-button"
+                style={{maxWidth: '250px'}}
                 onClick={handleClick}
                 fullWidth
               >
@@ -220,8 +227,6 @@ const CalorieForm = () => {
             </form>
           </>
         )}
-        
-
       </Box>
       {dailybarchart ? <DailyBarchart listedMeals={dailyCalorieInfos} /> : null}
     </>
