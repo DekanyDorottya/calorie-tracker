54a80a9ef0362c7a2d433936b0942468c26fb0e7
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-10-23 20:01:25 +0200

    Revert "add activityType and update the related fields"
    This reverts commit b3941920db1dff3f8a727c7a957b2e3f266103b5.

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewActivityDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewActivityDTO.java
@@ -1,8 +1,6 @@
 package org.codecool.fitnesstracker.fitnesstracker.controller.dto;
 
-import org.codecool.fitnesstracker.fitnesstracker.dao.model.ActivityType;
-
 import java.time.LocalDateTime;
 
-public record NewActivityDTO(ActivityType activityType, int calories) {
+public record NewActivityDTO(String activity, int calories) {
 }
